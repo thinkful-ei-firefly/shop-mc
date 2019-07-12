ace932f6a27dcdd90bfc9a615b156698cef5c2a0
Chris <50714992+cmpowers1981@users.noreply.github.com>
2019-07-12 09:43:05 -0400

    Add files via upload
    manual upload git wasn't  working

---
+++ script.js
@@ -0,0 +1,42 @@
+'use strict';
+
+
+$(document).ready (function() {
+    return `
+        <li>
+            <span class="shopping-item">${submittedText}</span>
+
+                <div class="shopping-item-toggle">
+                    <button class="shopping-item-controls">
+                        <span class='button-label'>check</span>
+                    </button>
+                    <button class="shopping-item-delete">
+                        <span class="button-label">delete</span>
+                    </button>
+            </div>
+        </li>
+     ';
+    }
+
+function createNewDiv(newDiv) {
+    $('ul').append(',newDiv);
+}
+
+function mainOne() {
+    $('#js-shopping-list-form').submit(function(event) {
+        event.preventDefault()
+        const submittedText = $('.js-shopping-list-entry').val();
+        newDiv(generateNewDiv)(submittedText));
+    })
+}
+$(mainOne());
+
+
+function toggleCheck() {
+    $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
+        let thisItem = $(this).closest('li').find('.shopping-item');
+        thisItem.toggleClass('shopping-item_checked');
+    })
+}
+$(toggleCheck());
+})
